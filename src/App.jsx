398121851a12b3827116c7a77dfe2ba5ce2848ae
Nitin Chakradhari <nitinchakradhari780@gmail.com>
2024-08-29 16:04:08 +0530

    used onclick and hooks concepts

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,56 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  let [count, setCount] = useState(0)
-  const addValue = () =>{
-    setCount(count++);
-  }
-  const removeValue = () =>{
-    setCount(count--);
-  }
+  let [color, setColor] = useState("olive")
+  
   return (
     <>
-      <h1>Using Hooks</h1>
-      <h3>Counter </h3>
-      <h4>Counter value {count}</h4>
-      <button onClick={addValue}>Add Value</button>
-      <br />
-      <br />
-      <button onClick={removeValue}>Remove Value</button>
+      <div className="w-full h-screen duration-200"
+      style={{backgroundColor: color}}>
+
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+
+          <div className="flex-wrap flex justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+
+            <button 
+            onClick={()=>{setColor("red")}}
+            className='outline-none px-4 py-1 founded-full text-white shadow-lg'
+            style={{backgroundColor: "red"}}
+            >Red</button>
+
+            <button 
+            onClick={()=>{setColor("green")}}
+            className='outline-none px-4 py-1 founded-full text-white shadow-lg'
+            style={{backgroundColor: "green"}}
+            >Green</button>
+
+            <button 
+            onClick={()=>{setColor("yellow")}}
+            className='outline-none px-4 py-1 founded-full text-white shadow-lg'
+            style={{backgroundColor: "yellow"}}
+            >yellow</button>
+
+            <button 
+            onClick={()=>{setColor("black")}}
+            className='outline-none px-4 py-1 founded-full text-white shadow-lg'
+            style={{backgroundColor: "black"}}
+            >black</button>
+
+            <button 
+            onClick={()=>{setColor("white")}}
+            className='outline-none px-4 py-1 founded-full text-white shadow-lg'
+            style={{backgroundColor: "white"}}
+            >White</button>
+
+            <button 
+            onClick={()=>{setColor("blue")}}
+            className='outline-none px-4 py-1 founded-full text-white shadow-lg'
+            style={{backgroundColor: "blue"}}
+            >Blue</button>
+
+          </div>
+        </div>
+      </div>
     </>
   )
 }
